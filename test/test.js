c96de6e0fab87e72d6aa3510c1d1663feca29dbd
Winter Zhong <wmzhong@users.noreply.github.com>
2018-03-01 20:15:32 +0800

    添加解析 html 字符串功能

--- test/test.js
+++ test/test.js
@@ -1,6 +1,10 @@
 // 函数入口
 function __main() {
     log('test')
+    // var a = parse(`<ul id="test-id" class="test-class"><li>1</li><li>2</li><li>3</li></ul>`)
+    
+    var a = analysis('<ul id="test-id" class="test-class"><li>1</li><li>2</li><li>3</li></ul>')
+    log('a', a)
 }
 
 __main()
