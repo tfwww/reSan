988cec22a54ccb0680b069177e142bcf4fcd073c
Winter Zhong <wmzhong@users.noreply.github.com>
2018-03-10 10:48:07 +0800

    完成数据绑定

--- src/welement.js
+++ src/welement.js
@@ -9,7 +9,9 @@ function Welement(option) {
     
     var rootEle = document.querySelector(opt.root)
     formatNode(rootEle)
-    dataWithDom(dataElePair, this.data)
+    dataWithDom(dataElePair, this.data)    
+    this.data.title = 'testTitle'
+    this.data.message = 'hell msg'
     var nodeList = Array.prototype.slice.call(rootEle.children)    
     // nodeList.forEach(v => {        
     //     var text = util.rmBrace(v.innerText)        
@@ -34,17 +36,34 @@ function tpl(match, name) {
     dataElePair[name]['dom'] = []
     return `<span ${mark}="${name}"></span>`
 }
-// 返回 {title: {value: 'hello', dom: node}}
+// 构造成这样的数据结构 {title: {value: 'hello', dom: node}}
 function dataWithDom(pair, srcData) {
     for (const key in pair) {        
         if (pair.hasOwnProperty(key)) {            
-            var attr = `[${mark}=${key}]`
+            var attr = `[${mark}=${key}]`            
             var eleList = document.querySelectorAll(attr)
+            log('elelist', eleList)
             pair[key]['dom'] = eleList
             pair[key]['value'] = srcData[key]
+
+            Object.defineProperty(srcData, key, {
+                set: function (newVal) {
+                    log('set newVal')
+
+                    Array.prototype.forEach.call(pair[key]['dom'], function(ele) {
+                        log('ele')
+                        ele.textContent = newVal
+                        pair[key].value = newVal                        
+                    })
+                },
+                get: function () {
+                    return pair[key].value
+                }
+            })
         }
     }
     log('pair', pair)
+    log('origin', dataElePair)
     return pair
 }
 
